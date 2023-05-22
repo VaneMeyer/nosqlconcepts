94d29e21c340444b8ed52a295716f017617855ae
VaneMeyer <meyer.van92@gmail.com>
2023-05-22 14:56:46 +0200

    added login form

---
+++ src/Sign-in/signIn.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./signin.css";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implement OAuth2 flow here to sign in student account
+  };
+
+  return (
+    <form id="sign-in" onSubmit={handleSubmit}>
+        <p>Sign in with your HRZ-Account</p>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Sign In</button>
+    </form>
+  );
+};
+
+export default SignIn;
