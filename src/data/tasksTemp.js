fac310edc72b9a23fcfec73f8e3af369a7e57077
VaneMeyer <meyer.van92@gmail.com>
2024-03-26 17:40:44 +0100

    added template for tasks

---
+++ src/data/tasksTemp.js
@@ -0,0 +1,45 @@
+//rename to tasks.js
+
+export const pgTasks = [
+  {
+    tasknumber: "Task 1",
+    topic: "",
+    subtasknumber: "",
+    maxtime: "",
+    description: "",
+    hint: "",
+  },
+];
+export const cassandraTasks = [
+  {
+    tasknumber: "Task 1",
+    topic: "",
+    subtasknumber: "",
+    maxtime: "",
+    description: "",
+    hint: "",
+  },
+  //add more task objekts
+];
+
+export const neo4jTasks = [
+  {
+    tasknumber: "Task 1",
+    topic: "",
+    subtasknumber: "",
+    maxtime: "",
+    description: "",
+    hint: "",
+  },
+];
+
+export const mongodbTasks = [
+  {
+    tasknumber: "Task 1",
+    topic: "",
+    subtasknumber: "",
+    maxtime: "",
+    description: "",
+    hint: "",
+  },
+];
