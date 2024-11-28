c46796eee207965dfc4f8bea3b200181f6f03c83
ranelkin <ranelkin23@gmail.com>
2024-11-28 12:54:57 +0100

    Removed unused import from cassandraSchema.jsx

--- src/components/cassandraSchema.jsx
+++ src/components/cassandraSchema.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 const dataModel = [
     {
