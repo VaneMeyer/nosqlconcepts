654605245cb596b3b5fc619909d4bc63e5b774e9
VaneMeyer <meyer.van92@gmail.com>
2025-04-11 09:52:38 +0200

    new layout and folder structure

--- src/old_components/Privateroute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoutes = () => {
-  let auth = localStorage.getItem("token");
-  return auth ? <Outlet /> : <Navigate to="/signin" replace />;
-};
-export default PrivateRoutes;
